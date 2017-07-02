66671cfe975087aefeef4fc5a03b56a79a675aa2
Canwen Xu <canwenxu@126.com>
2017-07-02 18:09:09 +0800

    update ext page

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function header(){
     $(".greyBg").prepend(header);
 }
 function footer(){
-    $(".greyBg").append($('<div class="mod-footer"><p>WHU教务魔改 ©JetRunner <a href="https://github.com/JetRunner/WHU-curr-mod">Github</a> 本扩展不会上传你的任何个人信息，并完全开源。若对此有任何疑虑，请直接移除本扩展。</p></div>').prepend($("#footer")));
+    $(".greyBg").append($('<div class="mod-footer"><p>Modified by WHU教务魔改 ©JetRunner <a href="https://github.com/JetRunner/WHU-curr-mod">Github</a></p></div>').prepend($("#footer")));
 }
 header();
 footer();
