7a1bb25844ad0ea08992744e2ad2522fc7130d8c
Canwen Xu <canwenxu@126.com>
2017-07-02 13:36:38 +0800

    echo "# WHU-curr-mod" >> README.md git init git add README.md git commit -m "first commit" git remote add origin https://github.com/JetRunner/WHU-curr-mod.git git push -u origin master

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function header(){
+    var header='<div class="index-header"><h1>武汉大学教务系统</h1><span>Mod</span></div>'
+    $(".greyBg").prepend(header);
+}
+function footer(){
+    $(".greyBg").append($('<div class="mod-footer"><p>WHU教务魔改 ©JetRunner <a href="https://github.com/JetRunner/WHU-curr-mod">Github</a> 本拓展不会上传你的任何个人信息，并完全开源。若对此有任何疑虑，请直接移除本拓展。</p></div>').prepend($("#footer")));
+}
+header();
+footer();
+//window.location.pathname
