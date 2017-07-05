730cbd9513cefac4fb5626480ef6a3c00ba4b339
Canwen Xu <canwenxu@126.com>
2017-07-05 14:23:40 +0800

    Latest Commit

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@ function footer(){
     $(".greyBg").append($('<div class="mod-footer"><p>Modified by WHU教务魔改 ©JetRunner <a href="https://github.com/JetRunner/WHU-curr-mod">Github</a></p></div>').prepend($("#footer")));
 }
 function button(){
+    $("#loginBtn").addClass("mod-btn").attr("value","登陆");
+    $("#captcha-img").click(function(){
+        $(".change-captcha").click();
+    });
     $(".course_shift").addClass("mod-btn").attr("value","列表模式");
 }
 header();
